62edca3eb4b835d447a2566d301df9905fa24369
Eric Anderson <elanderson@gmail.com>
2017-03-23 12:58:24 -0500

    Rename state to subregion and add migration. Migration will fail as column renames are not supported in SQLite

---
+++ Sqlite/Sqlite/Migrations/20170323174136_RenameContactStateToSubregion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EfSqlite.Migrations
+{
+    public partial class RenameContactStateToSubregion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "State",
+                table: "Contacts",
+                newName: "Subregion");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Subregion",
+                table: "Contacts",
+                newName: "State");
+        }
+    }
+}
